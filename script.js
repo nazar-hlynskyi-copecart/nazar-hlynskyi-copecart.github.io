29aa83ddb258954c692bbb8b24c8ada9975d53f1
Nazar Hlynskyi <nazar.hlynskyi@copecart.com>
2023-01-21 15:52:19 +0200

    c

--- script.js
+++ script.js
@@ -8,35 +8,61 @@ const ctx = canvas.getContext("2d");
 // Get the text output element
 const textOutput = document.getElementById("text-output");
 
+let mediaStream;
+
+// check if running on iOS
 const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+console.log(iOS);
 
 if(iOS){
-  // ask user permission to access the camera
-  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-    const videoTracks = stream.getVideoTracks();
-    window.adapter.browserShim.attachMediaStream(video, videoTracks[0]);
-    video.play();
-  });
+  // create a input file element
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'image/*';
+  input.capture = 'camera';
+  input.style.display = 'none';
+  input.id = 'cameraInput';
+
+  // create a take photo button
+  const button = document.createElement('button');
+  button.innerHTML = 'Take Photo';
+  button.onclick = function(){
+    input.click();
+  }
+
+  // append the input and the button to the body
+  document.body.appendChild(input);
+  document.body.appendChild(button);
+
+  // handle the file change event
+  input.onchange = function(event){
+    const file = event.target.files[0];
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    document.getElementById("picture").appendChild(img);
+  }
+
 }else{
   // Ask for user permission to access the webcam
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(stream => {
+      mediaStream = stream;
       video.srcObject = stream;
       video.play();
     })
     .catch(err => {
       console.error("Webcam access denied", err);
     });
-}
 
-// Function to take a picture
-function snap() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  ctx.drawImage(video, 0, 0);
-  canvas.style.display = "block";
-  video.style.display = "none";
-  // readTextFromImage();
+  // Function to take a picture
+  function snap() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0);
+    canvas.style.display = "block";
+    video.style.display = "none";
+    // readTextFromImage();
+  }
 }
 
 // Function to read text from image using OCR
