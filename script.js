61a7415cb5cb6dc4d9f4c2ace86543ffbfc4cd60
Nazar Hlynskyi <nazar.hlynskyi@copecart.com>
2023-02-23 11:12:07 +0200

    another code

--- script.js
+++ script.js
@@ -1,52 +1,7 @@
-const cameraFileInput = document.getElementById("cameraFileInput");
-const pictureFromCamera = document.getElementById("pictureFromCamera");
-
-cameraFileInput.addEventListener("change", function () {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    pictureFromCamera.setAttribute("src", window.URL.createObjectURL(this.files[0]));
-  } else {
-    const video = document.createElement("video");
-    video.setAttribute("autoplay", "");
-    video.setAttribute("muted", "");
-    video.setAttribute("playsinline", "");
-    const constraints = {
-      video: true
-    };
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(function (mediaStream) {
-        video.srcObject = mediaStream;
-        pictureFromCamera.appendChild(video);
-        video.play();
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-});
-
-function openCameraOrWebcam() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    cameraFileInput.click();
-  } else {
-    const constraints = {
-      video: true
-    };
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(function (mediaStream) {
-        const video = document.createElement("video");
-        video.setAttribute("autoplay", "");
-        video.setAttribute("muted", "");
-        video.setAttribute("playsinline", "");
-        video.srcObject = mediaStream;
-        pictureFromCamera.appendChild(video);
-        video.play();
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-}
-
-document.getElementById("openCameraOrWebcam").addEventListener("click", function() {
-  openCameraOrWebcam();
-});
+document
+  .getElementById("cameraFileInput")
+  .addEventListener("change", function () {
+    document
+      .getElementById("pictureFromCamera")
+      .setAttribute("src", window.URL.createObjectURL(this.files[0]));
+  });
