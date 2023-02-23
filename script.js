2d61031ac440e4fe5df867e6b2bbe3b7eff24637
Nazar Hlynskyi <nazar.hlynskyi@copecart.com>
2023-02-23 11:51:11 +0200

    another code

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function openCameraOrWebcam() {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // mobile device detected
+    document.querySelector('#cameraFileInput').click();
+  } else {
+    // desktop device detected
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      .then(stream => {
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        document.body.appendChild(video);
+        video.play();
+      })
+      .catch(error => console.log(error));
+  }
+}
+
+document.querySelector('#openCameraOrWebcam').addEventListener('click', openCameraOrWebcam);
+
 document
   .getElementById("cameraFileInput")
   .addEventListener("change", function () {
